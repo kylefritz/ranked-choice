416e6846c88b1ef8e08536a89ca7912f141d1626
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-21 11:05:42 -0400

    ranked choice vote fixture

--- test/models/election_test.rb
+++ test/models/election_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class ElectionTest < ActiveSupport::TestCase
-  test "round 1" do
+  test "first 100" do
+    the_votes = votes()[0..99]
+    assert_equal the_votes.size, 100
+
+    election = Election.new(the_votes.map(&:candidate_preference))
+    round1 = election.round_1_results
+    assert_equal round1, {"Cross"=>17, "Crum"=>13, "Heikler"=>19, "Stokes"=>25, "Westry"=>26}
+    assert_equal the_votes.size, round1.values.sum, 'count should be equal'
+  end
+
+  test "all fixture means 3-way tie for 3rd" do
     election = Election.new(votes().map(&:candidate_preference))
     round1 = election.round_1_results
-    assert_equal round1, {'Cross' => 1, 'Crum' =>1, 'Heikler' =>1, 'Stokes' => 1 , 'Westry' => 2}
+    assert_equal round1, {"Cross"=>19, "Crum"=>19, "Heikler"=>19, "Stokes"=>25, "Westry"=>26}
+    assert_equal votes().size, round1.values.sum, 'count should be equal'
   end
 end
