580224346f6f6e2bf125873d94cca9dabf3aacde
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-21 23:39:12 -0400

    votes fixture

--- lib/tasks/create-votes-fixture.rb
+++
@@ -1,8 +0,0 @@
-candidates = Candidate.pluck('last_name')
-100.times do |i|
-  puts "vote#{i}:"
-  vote = candidates.sample(candidates.size)
-  puts "  ordered_candidate_last_names: #{vote.join(',')}"
-  puts "  voted_by: voter#{i}"
-  puts ""
-end
