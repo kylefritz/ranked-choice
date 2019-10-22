ad2311c18a7509fad2ca90b7bbc532770f12e1b4
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-21 22:22:59 -0400

    first cut at RCV; might be some bugs

--- app/models/election.rb
+++ app/models/election.rb
@@ -4,22 +4,98 @@ class Election
     @votes = votes
   end
 
-  def round_1_results
-    # we migth mutate our copy here; make a duplicate
-    votes = @votes.dup
+  def winner_take_all_results
+    vote_tally = empty_vote_tally()
+    vote_tally = update_vote_tally_for_votes(vote_tally, @votes)
+    summarize_results(vote_tally)
+  end
 
-    # intialize a hash with each candidate set to 0 
-    votes.flatten.uniq.sort.zip([0] * 5).to_h.tap do |vote_tally|
+  def ranked_choice_results
+    round = 1
+    vote_tally = empty_vote_tally()
+    vote_tally = update_vote_tally_for_votes(vote_tally, @votes)
 
-      votes.each do |ranked_vote|
-        top_pick = ranked_vote[0]
-        vote_tally[top_pick] += 1
+    loop do
+      results = summarize_results(vote_tally)
+      Rails.logger.warn "round=#{round} #{results}"
+
+      if winner_decided?(vote_tally)
+        return results.tap do |results|
+          winner = results.max_by {|k,v| v}.first
+          Rails.logger.warn "decided on #{winner}"
+        end
       end
-
+      Rails.logger.warn "not decided; going to next."
+      round += 1
+      
+      vote_tally = instant_runoff(vote_tally, round)
+      if round > 10
+        throw "rounds #{round} too high "
+      end
     end
   end
 
-  def winner_decided?
+  def empty_vote_tally
+    # create empty vote_tally: a hash with each candidate set to 0 
+    candidate_names = @votes.flatten.uniq.sort
+    candidate_zeros = candidate_names.map {[]}
+    vote_tally = candidate_names.zip(candidate_zeros).to_h
+  end
+
+  def update_vote_tally_for_votes(vote_tally, votes)
+    # increment each candidate according to first choice
+    votes.each do |ranked_vote|
+      top_pick = ranked_vote[0]
+      if top_pick
+        vote_tally[top_pick].push(ranked_vote)
+      end
+    end
+    vote_tally
+  end
+
+  def summarize_results(vote_tally)
+    {}.tap do |summary|
+      vote_tally.each do |candidate_name, votes|
+        summary[candidate_name] = votes.size
+      end
+    end
+  end
+
+  def leading_fraction(vote_tally)
+    vote_summary = summarize_results(vote_tally)
+    leading_votes = vote_summary.values.max
+    votes_cast = vote_summary.values.sum
+    Float(leading_votes) / votes_cast
+  end
+
+  def winner_decided?(vote_summary)
+    leading_fraction(vote_summary) > 0.5
+  end
+
+  def instant_runoff(vote_tally, round)
+    vote_summary = summarize_results(vote_tally)
+    min_vote_count = vote_summary.values.min
+    Rails.logger.warn "round #{round}: min_vote_count #{min_vote_count}"
     
+    num_candidates_eliminated = 0
+    ballots_for_next_choice = []
+    vote_tally.each do |candidate, ballots|
+      if ballots.size == min_vote_count
+        num_candidates_eliminated += 1
+        ballots.each do |ballot|
+          ballot.shift() # this candidate has been eliminated
+          ballots_for_next_choice.push(ballot)
+        end
+        vote_tally.delete(candidate)
+        Rails.logger.warn "round #{round}: eliminated #{candidate}"
+      end
+    end
+
+    Rails.logger.warn "round #{round}: candidates_eliminated=#{num_candidates_eliminated}"
+    num_ballots_for_next_choice = ballots_for_next_choice.size / num_candidates_eliminated
+    srand 42 # set random seed for reproducibility
+    selected_ballots_for_next_choice = ballots_for_next_choice.sample(num_ballots_for_next_choice)
+    
+    update_vote_tally_for_votes(vote_tally, selected_ballots_for_next_choice)
   end
 end
--- test/models/election_test.rb
+++ test/models/election_test.rb
@@ -6,15 +6,28 @@ class ElectionTest < ActiveSupport::TestCase
     assert_equal the_votes.size, 100
 
     election = Election.new(the_votes.map(&:candidate_preference))
-    round1 = election.round_1_results
-    assert_equal round1, {"Cross"=>17, "Crum"=>13, "Heikler"=>19, "Stokes"=>25, "Westry"=>26}
-    assert_equal the_votes.size, round1.values.sum, 'count should be equal'
+    vote_talley = election.winner_take_all_results
+    assert_equal vote_talley, {"Cross"=>17, "Crum"=>13, "Heikler"=>19, "Stokes"=>25, "Westry"=>26}
+    assert_equal the_votes.size, vote_talley.values.sum, 'count should be equal'
   end
 
   test "all fixture means 3-way tie for 3rd" do
     election = Election.new(votes().map(&:candidate_preference))
-    round1 = election.round_1_results
-    assert_equal round1, {"Cross"=>19, "Crum"=>19, "Heikler"=>19, "Stokes"=>25, "Westry"=>26}
-    assert_equal votes().size, round1.values.sum, 'count should be equal'
+    vote_talley = election.winner_take_all_results
+    assert_equal vote_talley, {"Cross"=>19, "Crum"=>19, "Heikler"=>19, "Stokes"=>25, "Westry"=>26}
+    assert_equal votes().size, vote_talley.values.sum, 'count should be equal'
+  end
+
+  test "first 10" do
+    the_votes = votes()[0..9]
+    assert_equal the_votes.size, 10
+
+    election = Election.new(the_votes.map(&:candidate_preference))
+    vote_talley = election.winner_take_all_results
+    assert_equal vote_talley, {"Cross"=>0, "Crum"=>2, "Heikler"=>2, "Stokes"=>2, "Westry"=>4}
+    assert_equal the_votes.size, vote_talley.values.sum, 'count should be equal'
+
+    winner = election.ranked_choice_results.max_by {|k,v| v}.first
+    assert_equal winner, "Westry"
   end
 end
