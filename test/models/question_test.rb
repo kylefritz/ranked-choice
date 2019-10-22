ccc6269110d79fb338894c662adf873173b6b425
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-21 23:51:22 -0400

    cleanup tests

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class QuestionTest < ActiveSupport::TestCase
   test "question votes" do
     p = questions(:police)
-    assert_equal p.up_vote_count, 2
-    assert_equal p.down_vote_count, 1
-    assert_equal p.vote_count, 1
+    assert_equal 2, p.up_vote_count
+    assert_equal 1, p.down_vote_count
+    assert_equal 1, p.vote_count
   end
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
   test "candidate_preference" do
-    assert_equal votes(:vote1).candidate_preference[0], 'Crum'
+    assert_equal 'Crum', votes(:vote1).candidate_preference[0]
   end
 end
