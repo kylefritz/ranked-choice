521540a8993feafb6366e471eb842f62aeafeff8
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-23 02:38:11 -0400

    rename settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,15 +2,11 @@
 class Setting < RailsSettings::Base
   cache_prefix { "v1" }
 
-  field :voting_enable, default: "0", type: :boolean
-  field :results_enable, default: "0", type: :boolean
-  field :questions_enable, default: "0", type: :boolean
+  field :voting_enabled, default: true, type: :boolean
+  field :results_enabled, default: true, type: :boolean
+  field :questions_enabled, default: true, type: :boolean
 
   # Define your fields
   # field :host, type: :string, default: "http://localhost:3000"
-  # field :default_locale, default: "en", type: :string
-  # field :confirmable_enable, default: "0", type: :boolean
   # field :admin_emails, default: "admin@rubyonrails.org", type: :array
-  # field :omniauth_google_client_id, default: (ENV["OMNIAUTH_GOOGLE_CLIENT_ID"] || ""), type: :string, readonly: true
-  # field :omniauth_google_client_secret, default: (ENV["OMNIAUTH_GOOGLE_CLIENT_SECRET"] || ""), type: :string, readonly: true
 end
