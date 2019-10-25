aaa2cf077d0c76ce53a18d442f867ca100cd853c
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-25 13:38:17 -0400

    "test" for question votes

--- test/models/question_vote_test.rb
+++ test/models/question_vote_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class QuestionVoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "add random votes" do
+    q_ids = questions().pluck(:id)
+
+    q_ids.each do |id|
+      q = Question.find(id)
+      rand(16).times do 
+        is_upvote = [true, false].sample()
+        q.question_votes.create!(is_upvote: is_upvote, voted_by: 'test')
+      end
+    end
+    
+    # TODO: doesn't assert anything
+  end
 end
