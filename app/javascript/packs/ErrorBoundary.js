5449ca21017d226b3cbae56a10d9a4ee4ed20436
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-30 18:18:07 -0400

    show more context in ErrorBoundary

--- app/javascript/packs/ErrorBoundary.js
+++ app/javascript/packs/ErrorBoundary.js
@@ -19,16 +19,31 @@ export default class ErrorBoundary extends React.Component {
       const eventId = Sentry.captureException(error);
       this.setState({ eventId });
     });
+    this.setState({ error: error.message, stack: error.stack.toString() })
   }
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
+      const { eventId, error, stack } = this.state;
+      // render fallback UI
       return (
         <>
           <h2>There was an error in this voting software :(</h2>
-          <br />
-          <button onClick={() => Sentry.showReportDialog({ eventId: this.state.eventId })}>Tell us what happened!</button>
+          <div className="mt-3">
+            <button onClick={() => Sentry.showReportDialog({ eventId })}
+              className="btn-primary btn-lg btn-block" type="button">
+              Tell us what happened!
+            </button>
+          </div>
+          <p className="text-center">Then maybe try again or try back later?</p>
+          <div className="mt-5">
+            <code>{error}</code>
+          </div>
+          <div className="mt-2">
+            <code className="stack-trace">
+              {stack}
+            </code>
+          </div>
         </>
       );
     }
