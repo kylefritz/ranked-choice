a32736489a54a7766f614df51aef494181fffbb6
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-11-27 23:55:50 -0500

    import bable's polyfills

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// include bable pollyfills
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
