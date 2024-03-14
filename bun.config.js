d1411e633e931fc8e22266739a39643ccc376006
Kyle Fritz <kyle.p.fritz@gmail.com>
2024-03-14 08:33:03 -0400

    switch to bun

---
+++ bun.config.js
@@ -0,0 +1,42 @@
+import path from 'path';
+import fs from 'fs';
+
+const config = {
+  sourcemap: "external",
+  entrypoints: [
+    "app/javascript/application.js",
+    "app/javascript/questions.js",
+    "app/javascript/results.js",
+    "app/javascript/votes.js"
+  ],
+  outdir: path.join(process.cwd(), "app/assets/builds"),
+};
+
+const build = async (config) => {
+  const result = await Bun.build(config);
+
+  if (!result.success) {
+    if (process.argv.includes('--watch')) {
+      console.error("Build failed");
+      for (const message of result.logs) {
+        console.error(message);
+      }
+      return;
+    } else {
+      throw new AggregateError(result.logs, "Build failed");
+    }
+  }
+};
+
+(async () => {
+  await build(config);
+
+  if (process.argv.includes('--watch')) {
+    fs.watch(path.join(process.cwd(), "app/javascript"), { recursive: true }, (eventType, filename) => {
+      console.log(`File changed: ${filename}. Rebuilding...`);
+      build(config);
+    });
+  } else {
+    process.exit(0);
+  }
+})();
